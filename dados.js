46ce1756163b80c31591e61277c7fce6148f7a88
almocoes <159978185+almocoes@users.noreply.github.com>
2024-09-09 21:43:45 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Malhado",
+        descricao: "O gato malhado é um bom companheiro, mas também um animal independente que gosta muito de estar ao ar livre.",
+        link: "https://en.wikipedia.org/wiki/Tabby_cat",
+        tags: "malhado companheiro"
+    },
+    {
+        titulo: "Siamês",
+        descricao: "Conhecido por sua pelagem bicolor e olhos azuis intensos, o Siamês é um gato inteligente, vocal e muito apegado ao seu dono.",
+        link: "https://en.wikipedia.org/wiki/Siamese_cat",
+        tags: "siames siamês bicolor"
+    },
+    {
+
+        titulo: "Sphynx",
+        descricao: "O Sphynx é uma raça de gato sem pelo, conhecido por sua pele quente e macia ao toque. Apesar da aparência única, são gatos afetuosos e brincalhões.",
+        link: "https://en.wikipedia.org/wiki/Sphynx_cat",
+        tags: "sphynx pelo careca"
+    }
+];
